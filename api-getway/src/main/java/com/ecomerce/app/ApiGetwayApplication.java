364f9073482dce0b9ab6a6e4d391ab67878d8952
ElomariHana <elomarih5@gmail.com>
2025-03-13 22:28:12 +0000

    fisrt commit

---
+++ api-getway/src/main/java/com/ecomerce/app/ApiGetwayApplication.java
@@ -0,0 +1,11 @@
+package com.ecomerce.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGetwayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGetwayApplication.class, args);
+    }
+}
