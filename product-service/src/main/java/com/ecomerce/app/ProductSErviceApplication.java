eef9ef93db1d4505a5ee2e34e74b28380f03c289
ElomariHana <elomarih5@gmail.com>
2025-03-14 15:04:20 +0000

    feat: add product and order services

---
+++ product-service/src/main/java/com/ecomerce/app/ProductSErviceApplication.java
@@ -0,0 +1,11 @@
+package com.ecomerce.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductSErviceApplication {
+    public static void main(String args[]) {
+        SpringApplication.run(ProductSErviceApplication.class, args);
+    }
+}
